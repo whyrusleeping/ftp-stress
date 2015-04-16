b12319ea49bf528de59ee7f5caaf9666e85b7d16
Jeromy <jeromyj@gmail.com>
2015-04-15 20:04:39 -0700

    allow file lists

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	humanize "github.com/dustin/go-humanize"
+	"os"
 	//"github.com/jlaffaye/ftp"
 	ftp "code.google.com/p/ftp4go"
 	"io"
@@ -55,8 +57,22 @@ func StressReads(con *ftp.FTP, files []string, iters int) (int64, error) {
 	return nread, nil
 }
 
+func GetFileList(file string) ([]string, error) {
+	fi, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var out []string
+	scan := bufio.NewScanner(fi)
+	for scan.Scan() {
+		out = append(out, scan.Text())
+	}
+	return out, nil
+}
+
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
+	runtime.GOMAXPROCS(4)
 
 	host := flag.String("host", "localhost", "hostname of ftp server")
 	port := flag.Int("port", 21, "port of ftp server")
@@ -65,16 +81,29 @@ func main() {
 	pass := flag.String("pass", "password", "password")
 	iters := flag.Int("iter", 1, "number of iterations per thread")
 	rfile := flag.String("file", "", "number of iterations per thread")
+	flist := flag.String("file-list", "", "file containing list of files to read")
 
 	flag.Parse()
 
-	if *rfile == "" {
-		fmt.Println("Please specify a file to read with `-file=X`")
-		return
+	var files []string
+	if *flist != "" {
+		fs, err := GetFileList(*flist)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		files = fs
+	}
+
+	if files == nil {
+		if *rfile == "" {
+			fmt.Println("Please specify a file to read with `-file=X` or `-file-list=Y`")
+			return
+		}
+		files = []string{*rfile}
 	}
 
 	// temp
-	files := []string{*rfile}
 
 	donech := make(chan int64)
 	starttime := time.Now()
